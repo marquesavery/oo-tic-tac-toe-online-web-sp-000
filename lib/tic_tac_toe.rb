62f7e5390dfdfc50cd97952c1cf638583d235b57
Marques Avery <averymarques4@gmail.com>
2020-08-20 18:17:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,5 +36,9 @@ class TicTacToe
     @board[location] != " " && @board[location] != ""
   end
 
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+
 
 end
